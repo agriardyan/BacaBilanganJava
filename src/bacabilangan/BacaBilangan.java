709e90b517ba4b9cb9bcb2b398a70f1131a422df
Agustinus Agri Ardyan <superbro.ardyan@gmail.com>
2015-05-09 20:44:02 +0700

    commit pertama

---
+++ src/bacabilangan/BacaBilangan.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bacabilangan;
+
+/**
+ * @author yanick.rochon@gmail.com [original author]
+ * @author agriardyan <aagriard@gmail.com>
+ */
+public class BacaBilangan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        NumberToWords.main(args);
+        BilanganKeHuruf.main(args);
+    }
+    
+}
